e73fc4bb6aa31661f964e9a8ec4cd837fed39642
Cutlas-Beep <kje2254@gmail.com>
2022-11-14 12:54:31 +0900

    1564, 1096

--- 22-11/11-12/1564.c
+++ 22-11/11-12/1564.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 
 int a, b;
-int ans;
 
-int gcd(){
-	if(b==0)return a;
-	else return gcd(b, a%b);
+int gcd(){ 
+    int ans;
+    if(a==b) // 두 수가 같으면
+        return a;
+    for(int i=1; i<a; i++){ //모든 약수를 구함  
+        if(a%i==0 && b%i==0) //마지막으로 구해진 약수가 최대공약수
+            ans = i;
+    }
+    return ans;
 }
-
 
 int main(){
     scanf("%d%d", &a, &b);
     printf("%d\n", gcd(a, b)); //a와 b의 최대 공약수 구하기
+    return 0;
 }
